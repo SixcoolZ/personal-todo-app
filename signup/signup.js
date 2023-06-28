d97bce3f9b9d1f35b3a462854593e45f74cfc7eb
utibe <prophetuty@gmail.com>
2023-06-28 14:05:27 +0100

    created a folder for signup and worked on the signup interface

---
+++ signup/signup.js
@@ -0,0 +1,24 @@
+const formEle = document.forms[0];
+
+formEle.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries())
+    
+    // SignUp(formEle.method, formEle.action, data)
+    SignUp(data);
+
+    formEle.reset();
+})
+
+
+function SignUp(data){
+    fetch("https://todo-ts-cs5q.onrender.com/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        body: JSON.stringify(data)
+    }).then((res) => res.json())
+    .then((data) => (window.location.href = "/Add/addTodo.html"))
+}
